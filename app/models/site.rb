0f979c4ef4a0a72a70c55d6e57f6515394791e12
Hendrik Vermeersch <hendrik.vermeersch@protonmail.com>
2021-03-09 15:20:57 +0300

    added geocode validations to site model

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,4 +3,6 @@ class Site < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :address, message: "should be unique per address" }
   validates :address, presence: true
   validates :description, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
