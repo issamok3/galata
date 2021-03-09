c4674c949dfbd80dfcd16eb1e15f067a9feea9d0
Hendrik Vermeersch <hendrik.vermeersch@protonmail.com>
2021-03-09 17:52:47 +0300

    added map to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
     @sites = Site.all
+    @markers = @sites.geocoded.map do |site|
+      {
+        lat: site.latitude,
+        lng: site.longitude
+      }
+    end
   end
 end
