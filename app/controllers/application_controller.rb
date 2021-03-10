f6bc77eef89f7a10a5646db4c273d935b8f16aa6
sevil hatipoglu <sevilhatipogluu93@gmail.com>
2021-03-10 09:39:04 +0000

    Change form for sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :address, :locatable, :username])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :address, :locatable, :username])
+  # end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+  #   user_params.permit({ locatable: [] }, :email, :password, :password_confirmation, :usernmae, :full_name, :address)
+  #   end
+  # end
 end
