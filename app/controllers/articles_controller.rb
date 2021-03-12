5869f212e2242cb22b2e9c6d41ce9b72b34b8ffd
Hendrik Vermeersch <hendrik.vermeersch@protonmail.com>
2021-03-12 13:31:59 +0300

    made create action in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_site, except: :new
+  before_action :set_site, except: [:new, :create]
 
   def index
     @articles = Article.where(site: @site)
@@ -12,9 +12,25 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @site = Site.find(params[:article][:site_id])
+    @article = Article.new(article_params)
+    @article.site = @site
+    @article.user = current_user
+    if @article.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_site
     @site = Site.find(params[:site_id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description, :body)
   end
 end
