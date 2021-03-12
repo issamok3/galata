c9a94b84b82b7f316d0ab8f4106148b1145f7e88
Hendrik Vermeersch <hendrik.vermeersch@protonmail.com>
2021-03-12 11:55:25 +0300

    set up new article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,15 @@
 class ArticlesController < ApplicationController
-  before_action :set_site
+  before_action :set_site, except: :new
 
   def index
     @articles = Article.where(site: @site)
   end
 
   def show
+  end
+
+  def new
+    @article = Article.new
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,13 @@ Rails.application.routes.draw do
     #   get :photos
     # end
     resources :articles, only: [:index, :show]
-    resources :photos, only: :index
-    resources :audios, only: :index
-    resources :videos, only: :index
+    resources :photos, only: [:index, :new]
+    resources :audios, only: [:index, :new]
+    resources :videos, only: [:index, :new]
   end
   resource :dashboard, only: :show
+  resources :articles, only: [:new, :create]
+  # resources :photos, only: :new
+  # resources :audios, only: :new
+  # resources :videos, only: :new
 end
