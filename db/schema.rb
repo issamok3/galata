364382f893b1e25f0fb7eecf151707c57ab738d3
Hendrik Vermeersch <hendrik.vermeersch@protonmail.com>
2021-03-14 15:11:14 +0300

    created route, controller actions and views for new feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_120137) do
+ActiveRecord::Schema.define(version: 2021_03_14_115154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2021_03_12_120137) do
     t.text "description"
     t.string "type"
     t.string "url"
+    t.string "spotify_uri"
     t.index ["site_id"], name: "index_contents_on_site_id"
     t.index ["user_id"], name: "index_contents_on_user_id"
   end
