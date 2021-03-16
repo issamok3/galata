3bde5a58c13f74ee7b0db11ab60dd094d8053911
Hendrik Vermeersch <hendrik.vermeersch@protonmail.com>
2021-03-16 20:15:08 +0300

    fixed bugs in routes

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -12,7 +12,6 @@ const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
     if (marker.image_url) {
-      console.log(marker)
       const element = document.createElement('div');
       element.className = 'user-marker';
       element.style.backgroundImage = `url('${marker.image_url}')`;
@@ -33,7 +32,6 @@ const addMarkersToMap = (map, markers) => {
 };
 
 const fitMapToMarkers = (map, markers) => {
-  console.log(markers);
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
