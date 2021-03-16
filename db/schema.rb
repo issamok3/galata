1e4aab1592fba3c1395e4fb4fdc737501b8c3ccd
sevil hatipoglu <sevilhatipogluu93@gmail.com>
2021-03-16 07:33:39 +0000

    notification color changed a red color

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_094412) do
+ActiveRecord::Schema.define(version: 2021_03_15_141055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,14 @@ ActiveRecord::Schema.define(version: 2021_03_15_094412) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "feedback"
+    t.bigint "content_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["content_id"], name: "index_reviews_on_content_id"
+  end
+
   create_table "sites", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -126,6 +134,7 @@ ActiveRecord::Schema.define(version: 2021_03_15_094412) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
@@ -160,4 +169,5 @@ ActiveRecord::Schema.define(version: 2021_03_15_094412) do
   add_foreign_key "messages", "users"
   add_foreign_key "participants", "conversations"
   add_foreign_key "participants", "users"
+  add_foreign_key "reviews", "contents"
 end
