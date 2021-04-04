ddc9f4f3bb63de2d7823aae179442c7c7b747ca4
sevil hatipoglu <sevilhatipogluu93@gmail.com>
2021-04-04 15:07:04 +0300

    fixed bug in login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :address, :locatable, :username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :address, :locatable, :username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :location, :locatable, :username, :range])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :location, :locatable, :username, :range])
   end
 
   def default_url_options
